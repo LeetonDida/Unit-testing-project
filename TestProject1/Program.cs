57638a00982d63254bb86122492398955172abaa
Leeton Dida <leetontafaradida@gmail.com>
2022-03-02 10:45:41 +0000

    Testing

---
+++ TestProject1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My test program");
+        }
+    }
+
+    public class math_algorithm
+    {
+        public int sqr(int x)
+        {
+            return x * x;
+        }
+    }
+}
